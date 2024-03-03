386c2ee93b25dbdb13ea4d3c1942af6e69abf19d
Jiro Matsuzawa <matsuzawa.jr@gmail.com>
2024-03-03 14:32:28 +0900

    Add simple data processing

--- bitfield/bitfield.go
+++ bitfield/bitfield.go
@@ -6,13 +6,24 @@ import (
 )
 
 func Unmarshal(data []byte, v any) error {
-	// Check v is a pointer to a struct
-	if v == nil {
-		return errors.New("v must be a pointer to a struct")
+	rv := reflect.ValueOf(v)
+	// Note: reflect.ValueOf(nil).Kind() == reflect.Invalid
+	if rv.Kind() != reflect.Ptr || rv.IsNil() || rv.Elem().Kind() != reflect.Struct {
+		return errors.New("v must be a non-nil pointer to a struct")
 	}
-	rt := reflect.TypeOf(v)
-	if rt.Kind() != reflect.Ptr || rt.Elem().Kind() != reflect.Struct {
-		return errors.New("v must be a pointer to a struct")
+
+	rt := reflect.TypeOf(v).Elem()
+	for i := 0; i < rt.NumField(); i++ {
+		tf := rt.Field(i)
+		vf := rv.Elem().Field(i)
+		if tf.Type.Kind() == reflect.Uint8 {
+			vf.SetUint(uint64(data[i]))
+		}
+		if tf.Type.Kind() == reflect.Uint32 {
+			v := uint32(data[i+3])<<24 | uint32(data[i+2])<<16 | uint32(data[i+1])<<8 | uint32(data[i])
+			vf.SetUint(uint64(v))
+		}
 	}
+
 	return nil
 }
--- bitfield/bitfield_test.go
+++ bitfield/bitfield_test.go
@@ -4,6 +4,44 @@ import (
 	"testing"
 )
 
+func TestUnmarshalPlainStruct(t *testing.T) {
+	// Setup
+	var v struct {
+		N uint8
+	}
+
+	// Exercise
+	err := Unmarshal([]byte{0xA5}, &v)
+
+	// Verify
+	if err != nil {
+		t.Errorf("Unmarshal() = %v; want nil", err)
+	}
+
+	if v.N != 0xA5 {
+		t.Errorf("Unmarshal() = %v; want 0xA5", v.N)
+	}
+}
+
+func TestUnmarshalPlainStructUint32(t *testing.T) {
+	// Setup
+	var v struct {
+		N uint32
+	}
+
+	// Exercise
+	err := Unmarshal([]byte{0x01, 0x23, 0x45, 0x67}, &v)
+
+	// Verify
+	if err != nil {
+		t.Errorf("Unmarshal() = %v; want nil", err)
+	}
+
+	if v.N != 0x67452301 {
+		t.Errorf("Unmarshal() = %v; want 0x67452301", v.N)
+	}
+}
+
 func TestUnmarshalError(t *testing.T) {
 	var integer int
 	testCases := map[string]struct {
