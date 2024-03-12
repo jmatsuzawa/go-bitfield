155955b86cc550f885ac1848876488cfd1861e02
Jiro Matsuzawa <matsuzawa.jr@gmail.com>
2024-03-12 12:28:52 +0900

    Extract error types to another file

--- bitfield/bitfield.go
+++ bitfield/bitfield.go
@@ -6,26 +6,6 @@ import (
 	"strconv"
 )
 
-// An TypeError describes an invalid type passed to [Unmarshal].
-// (The argument to [Unmarshal] must be a non-nil pointer to a struct.)
-type TypeError struct {
-	Type    reflect.Type
-	problem string
-}
-
-func (e *TypeError) Error() string {
-	return "bitfield: " + e.problem
-}
-
-type FieldError struct {
-	Field   reflect.StructField
-	problem string
-}
-
-func (e *FieldError) Error() string {
-	return "bitfield: " + e.problem + " (" + e.Field.Name + " " + e.Field.Type.String() + " `" + string(e.Field.Tag) + "`)"
-}
-
 func Unmarshal(data []byte, v any, opts ...Option) error {
 	if err := validateUnmarshalType(v); err != nil {
 		return err
